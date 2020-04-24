b4e2db927e59d546671120c8a82d75dc55455502
Ahmad Awais <me@AhmadAwais.com>
2020-04-25 01:32:07 +0500

    🐛 FIX: Extraneous pkgs

--- utils/init.js
+++ utils/init.js
@@ -1,12 +1,11 @@
 const welcome = require('cli-welcome');
 const checkNode = require('cli-check-node');
 const pkgJSON = require('./../package.json');
-const updateNotifier = require('update-notifier');
 const unhandledError = require('cli-handle-unhandled');
 
 module.exports = async () => {
 	unhandledError();
-	checkNode(`12`);
+	checkNode(`10`);
 	welcome(`ramadan-cli`, `by Awais.dev\n${pkgJSON.description}`, {
 		bgColor: `#81EF96`,
 		color: `#FFFFFF`,
@@ -14,9 +13,4 @@ module.exports = async () => {
 		clear: true,
 		version: `v${pkgJSON.version}`
 	});
-	updateNotifier({
-		pkg: pkgJSON,
-		shouldNotifyInNpmScript: true,
-		updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week.
-	}).notify({isGlobal: true});
 };
