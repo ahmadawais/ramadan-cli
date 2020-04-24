7fb353acba288466f7d0c0ccd364c74da011e988
Ahmad Awais <me@AhmadAwais.com>
2020-04-25 01:08:42 +0500

    👌 IMPROVE: Lingo

--- utils/theEnd.js
+++ utils/theEnd.js
@@ -3,6 +3,11 @@ const {dim} = require('chalk');
 
 module.exports = async () => {
 	console.log(
+		dim(
+			`\n${sym.info} City: Lahore. Times for Fiqh Sunni\nFiqh Jafria: Sehar -10 mins | Iftar time +10 mins`
+		)
+	);
+	console.log(
 		`\n${sym.success} ${dim(
 			`Star the repo for updates → https://git.io/ramadan-cli`
 		)}\n${sym.info} ${dim(
