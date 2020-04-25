e5e52fcf944e65ac7066ebf0174e23517da5cc46
Ahmad Awais <me@AhmadAwais.com>
2020-04-26 02:20:05 +0500

    ‼️ BREAKING: Date Diff

--- utils/dateDiff.js
+++
@@ -1,7 +0,0 @@
-module.exports = (first, second) => {
-	// Take the difference between the dates and divide by milliseconds per day.
-	// Round to nearest whole number to deal with DST.
-	return Math.round(
-		(new Date(second) - new Date(first)) / (1000 * 60 * 60 * 24)
-	);
-};
--- utils/print.js
+++ utils/print.js
@@ -1,7 +1,7 @@
 const sym = require('log-symbols');
 const Table = require('cli-table3');
-const dateDiff = require('./dateDiff');
 const green = require('./green');
+const {DateTime} = require('luxon');
 const wishEid = `${sym.success} Eid Mubarak.\nRamadan is already over. Hope you had a fun time on Eid.\n`;
 
 module.exports = ({all, city}) => {
@@ -9,24 +9,28 @@ module.exports = ({all, city}) => {
 	const data = require(`../data/${city}.json`);
 
 	// Find the current roza.
-	const firstRoza = '2020-04-25';
-	const today = new Date().toISOString().substring(0, 10);
-	const rozaNumber = dateDiff(firstRoza, today);
+	const firstRoza = DateTime.fromISO('2020-04-25');
+	const today = DateTime.local();
+	const todayISO = DateTime.local().toISODate();
+	const rozaNumber = Math.floor(today.diff(firstRoza, 'days').as('days'));
 
 	// Still ramadan?
 	if (rozaNumber > 30) {
 		console.log(wishEid);
 	} else {
-		const roza = rozaNumber > 1 ? data[rozaNumber] : data[0];
+		const roza = rozaNumber > 0 ? data[rozaNumber] : data[0];
 
 		// Print table.
 		const table = new Table({
-			head: [green('Roza'), green('Sehar'), green('Iftar')]
+			head: [green('Roza'), green('Sehar'), green('Iftar'), green('Date')]
 		});
 
 		// All or one.
-		all && data.map(day => table.push([day.no, day.sehar, day.iftar]));
-		!all && table.push([roza.no, roza.sehar, roza.iftar]);
+		all &&
+			data.map(day =>
+				table.push([day.no, day.sehar, day.iftar, todayISO])
+			);
+		!all && table.push([roza.no, roza.sehar, roza.iftar, todayISO]);
 
 		// Do it.
 		console.log(table.toString());
