776b145421c886198b76c67dd4db33957901556c
Mian Muhammad <mianmuhammad80@gmail.com>
2020-05-13 16:41:54 +0500

    Revert "Revert "📦 NEW: added 182 new cities""
    This reverts commit 56b285c11f881a23ef7bdedbe90487329b500243.

--- utils/cities.js
+++ utils/cities.js
@@ -10,5 +10,18 @@ module.exports = [
 	`faisalabad`,
 	`gujranwala`,
 	`sialkot`,
-	`ghotki`
+	`ghotki`,
+	`abbottabad`,
+	`ahmadpur-sial`,
+	`arifwala`,
+	`athara-hazari`,
+	`attock`,
+	`awaran`,
+	`badin`,
+	`bahawalnagar`,
+	`bahawalpur`,
+	`bajaur-agency`,
+	`bannu`,
+	`barkhan`,
+	`bat-khela`, `batagram`, `bhakkar`, `bhalwal`, `bhimbar`, `bolan`, `buner`, `burewala`, `chagai`, `chakwal`, `chaman`, `charsadda`, `chichawatni`, `chiniot`, `chishtian`, `chitral`, `chunian`, `dadu`, `daggar`, `dalbandin`, `dargai`, `daska`, `dera-bugti`, `dera-ghazi-khan`, `dera-ismail-khan`, `dinga`, `dir`, `dir-upper`, `dunga-bunga`, `faqirwali`, `farooqabad`, `firozabad`, `ghizer`, `gilgit`, `gojra`, `gujar-khan`, `gujrat`, `gwadar`, `hafizabad`, `hala`, `hangu`, `haripur`, `harnai`, `harunabad`, `hasan-abdal`, `hasilpur`, `jacobabad`, `jafarabad`, `jalalpur-jattan`, `jalalpur-pirwala`, `jampur`, `jamshoro`, `jaranwala`, `jatoi`, `jhang`, `jhelum`, `kahuta`, `kalat`, `karak`, `karianwala`, `kashmore`, `kassowal`, `kasur`, `khairpur`, `khanewal`, `khanpur`, `kharan`, `kharian`, `khewra`, `khushab`, `khuzdar`, `khyber-agency`, `kohat`, `kohistan`, `kohlu`, `kot-addu`, `kotli`, `kotli-loharan`, `kurram-agency`, `ladhewala-waraich`, `lakki-marwat`, `lala-musa`, `landi-kotal`, `larkana`, `lasbela`, `leiah`, `liaquatpur`, `lodhran`, `loralai`, `mailsi`, `malakand`, `malakwal`, `mananwala`, `mandi-bahauddin`, `mansehra`, `mardan`, `mastung`, `matiari`, `mehar`, `mehrabpur`, `mian-channun`, `mianwali`, `minchinabad`, `mingaora`, `miranshah`, `mirpur`, `mirpur-khas`, `mohmand-agency`, `muridke`, `murree`, `muzaffarabad`, `muzaffargarh`, `nankana-sahib`, `narowal`, `nasirabad`, `naudero`, `naushahro-firoz`, `nawabshah`, `north-waziristan-agency`, `nowshera`, `nowshera-virkan`, `nushki`, `okara`, `orakzai-agency`, `pakpattan`, `panjgur`, `pasni`, `pattoki`, `pindi-gheb`, `pir-mahal`, `pishin`, `qila-abdullah`, `qila-saifullah`, `rahimyar-khan`, `rajanpur`, `rawalakot`, `sadiqabad`, `sahiwal`, `sakrand`, `sanghar`, `sarai-alamgir`, `sargodha`, `shahdadpur`, `shangla`, `sharqpur`, `sheikhupura`, `shikarpur`, `sibi`, `skardu`, `south-waziristan-agency`, `sukkur`, `swabi`, `swat`, `talagang`, `tando-adam`, `tando-allahyar`, `tando-muhammad-khan`, `tank`, `taxila`, `tharparkar`, `thatta`, `thul`, `toba-tek-singh`, `turbat`, `umarkot`, `vehari`, `wah-cantonment`, `wazirabad`, `zhob`, `ziarat`
 ];
